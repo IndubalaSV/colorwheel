1fd09053317804fc3d7a4228deff2de0f819910d
Indu <indubalaselvaraj@gmail.com>
2024-09-18 10:29:23 +0530

    add a checkbox to change the page bg color

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 function submitForm() {
+    // add fallback values as 0 to avoid errors
     const red = document.getElementById('red').value || 0;
     const green = document.getElementById('green').value || 0;
     const blue = document.getElementById('blue').value || 0;
+    const pagebg = document.getElementById('pagebg').checked
     document.getElementById('colorbox').style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-    //document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    if (pagebg === true) {
+        document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    } else {
+        // This will reset it to the default background color
+        document.body.style.backgroundColor = '';
+    }
 }
